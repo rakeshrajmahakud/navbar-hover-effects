3b6cb31818a0877f13e5deda426bde0658dc8b27
rakeshrajmahakud <rakeshraj.mahakud03@gmail.com>
2023-01-25 23:54:42 +0530

    navbar design 1

---
+++ index.js
@@ -0,0 +1,15 @@
+let arr = [true,false,1,0,undefined,5,""];
+let arr1=arr.filter(e => e)
+
+
+    let arr2=[1,2,3,4];
+    let arr3=[5,6,7,8]
+    // [1,5,,2,6,4,8]
+    let result = [];
+    for(let i = 0; i < arr2.length; i++){
+        result.push(arr2[i]);
+        result.push(arr3[i]);
+    }
+    // console.log(result);
+
+    let arr9 = [1,2,3,4,5,2,3]
